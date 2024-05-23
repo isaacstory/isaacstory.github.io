07ad19fa797ec94c58043ff4a10a6fce1b381216
Tony Lampada <tonylampada@gmail.com>
2024-05-22 22:51:59 -0300

    progress - roundtrip talking to assistance ok

--- gpt/adapters/agents.py
+++ gpt/adapters/agents.py
@@ -1,20 +1,20 @@
+import os
+import requests
 from openai import OpenAI
 client = OpenAI()
 
 def get_or_create(agent, agent_instructions):
-    try:
-        existing_agent = get_assistant_by_name(agent)
-        return existing_agent
-    except Exception as e:
-        if 'not found' in str(e):
-            new_agent = client.beta.assistants.create(
-                model="gpt-4",
-                name=agent,
-                instructions=agent_instructions
-            )
-            return new_agent
-        else:
-            raise
+    existing_agent = get_assistant_by_name(agent)
+    if existing_agent:
+        return existing_agent.id
+    else:
+        new_agent = client.beta.assistants.create(
+            model='gpt-4o',
+            name=agent,
+            instructions=agent_instructions,
+            tools=[{'type':'code_interpreter'}, {'type':'file_search'}]
+        )
+        return new_agent.id
 
 def get_assistant_by_name(agent):
     assistants = list(client.beta.assistants.list())
@@ -24,8 +24,52 @@ def get_assistant_by_name(agent):
     return assistants[0]
 
 def get_yaml_response(agent_id, prompt, filein):
-    pass
+    file_id = upload_file(filein)
+    thread = client.beta.threads.create()
+    message = client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=prompt,
+        attachments=[{'file_id': file_id, 'tools': [{'type':'code_interpreter'}, {'type':'file_search'}]}]
+    )
+    run = client.beta.threads.runs.create_and_poll(
+        thread_id=thread.id,
+        assistant_id=agent_id,
+        # instructions="Please address the user as Jane Doe. The user has a premium account."
+    )
+    if run.status == 'completed': 
+        messages = client.beta.threads.messages.list(
+            thread_id=thread.id
+        )
+        response = list(messages)[0].content[0].text.value
+    else:
+        response = run.status
+    delete_file(file_id)
+    return response
 
-def save_yaml_response(filein, dirout, prompt, response):
-    pass
+def upload_file(filepath):
+    headers = {
+        'Authorization': f'Bearer {os.getenv("OPENAI_API_KEY")}',
+        # 'Content-Type': 'multipart/form-data'
+    }
+    with open(filepath, 'rb') as file:
+        # read file as bytes
+        filebytes = file.read()
+        files = {
+            'file': ('a.pdf', filebytes),
+            'purpose': (None, 'assistants')
+        }
+        response = requests.post('https://api.openai.com/v1/files', headers=headers, files=files)
+        return response.json()['id']
+
+def delete_file(file_id):
+    headers = {
+        'Authorization': f'Bearer {os.getenv("OPENAI_API_KEY")}'
+    }
+    response = requests.delete(f'https://api.openai.com/v1/files/{file_id}', headers=headers)
+    return response.status_code
+
+
+def uploadfiletoopenai(path):
+    pass    
 
--- gpt/ask.py
+++ gpt/ask.py
@@ -1,6 +1,7 @@
 from adapters import agents
 import dotenv
 import os
+import re
 
 dotenv.load_dotenv()
 hash = os.getenv("HASH")
@@ -8,15 +9,13 @@ _thisdir = os.path.dirname(os.path.abspath(__file__))
 dirout = f"{_thisdir}/../{hash}/yamlout"
 
 def main():
-    ask_and_save("summarizer", "exam", f"{_thisdir}/../{hash}/drive/Exams/20231217 - GPL - CSAP.pdf", dirout)
+    ask_and_save("summarizer", "summarize_exam", f"{_thisdir}/../{hash}/drive/Exams/20231217 - GPL - CSAP.pdf", dirout)
 
 def ask_and_save(agent, prompt, filein, dirout):
     agent_instructions = load_instructions(agent)
     agent_id = agents.get_or_create(agent, agent_instructions)
-    print(agent_id)
-
-    # response = agents.get_yaml_response(agent_id, load_prompt(prompt), filein)
-    # save_yaml_response(filein, dirout, prompt, response)
+    response = agents.get_yaml_response(agent_id, load_prompt(prompt), filein)
+    save_yaml_response(filein, dirout, prompt, response)
     # pass
 
 def load_instructions(agent):
@@ -24,11 +23,20 @@ def load_instructions(agent):
         return file.read()
 
 def load_prompt(prompt):
-    pass
+    with open(f"{_thisdir}/prompts/{prompt}.txt", "r") as file:
+        return file.read()
 
 def save_yaml_response(filein, dirout, prompt, response):
+    yaml_response = extract_code_blocks(response)[0]
+    filename = filein.split('/')[-1]
+    with open(f"{dirout}/{prompt}-{filename}.yaml", "w") as file:
+        file.write(yaml_response)
     pass
+
+def extract_code_blocks(markdown_text):
+    code_block_pattern = re.compile(r'```(?:\w+)?\n(.*?)\n```', re.DOTALL)
+    code_blocks = [block.strip() for block in code_block_pattern.findall(markdown_text)]
+    return code_blocks
 
 if __name__ == "__main__":
     main()
-
