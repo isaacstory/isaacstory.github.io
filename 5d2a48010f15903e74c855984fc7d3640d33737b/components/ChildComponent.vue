71b8b00bcf5923fd66721902449331e6469070cb
Tony Lampada <tonylampada@gmail.com>
2024-05-27 10:33:06 -0300

    vue app

---
+++ 5d2a48010f15903e74c855984fc7d3640d33737b/components/ChildComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="hello">Goodbye {{who}}</div>
+  </template>
+  
+  <script>
+  module.exports = {
+    data() {
+        return {
+            who: 'world'
+        }
+    }
+  }
+  </script>
+  
+  <style>
+  .hello {
+    background-color: #bbb;
+  }
+  </style>
---
+++ 5d2a48010f15903e74c855984fc7d3640d33737b/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="goodbye">Hello {{who}}</div>
+    <child-component></child-component>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  data() {
+      return {
+          who: 'world'
+      }
+  },
+  components: {
+    'child-component': httpVueLoader('./ChildComponent.vue')
+  },
+}
+</script>
+
+<style>
+.goodbye {
+  background-color: #ffe;
+}
+</style>
