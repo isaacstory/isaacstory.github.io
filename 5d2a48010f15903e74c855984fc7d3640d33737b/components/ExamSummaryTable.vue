efc15cb43fc5349b6df1a47de738e6e4558ee8ed
Tony Lampada <tonylampada@gmail.com>
2024-05-30 01:01:35 -0300

    page v1 with exams summary

---
+++ 5d2a48010f15903e74c855984fc7d3640d33737b/components/ExamSummaryTable.vue
@@ -0,0 +1,124 @@
+<template>
+    <div>
+        <section class="search-section">
+            <input type="text" id="searchInput" v-model="searchQuery" placeholder="Filter exams by date, doctor, or purpose...">
+        </section>
+
+        <section class="records">
+            <h2>Medical Records</h2>
+            <table id="recordsTable">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Exam</th>
+                        <th>Doctor</th>
+                        <th>Summary</th>
+                        <th>File</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- Example Record -->
+                    <tr v-for="item in filteredExams" :key="item.file">
+                        <td style="min-width: 62px;">{{ item.sample_date }}</td>
+                        <td>{{ item.file }}</td>
+                        <td>{{ item.requested_by }}</td>
+                        <td>{{ item.assessmentSummary.join(', ') }}</td>
+                        <td><a :href="'drive/exams/' + item.file" target="_blank"><img :src="fileIcon(item)" alt="PDF"></a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+    </div>
+  </template>
+  
+<script>
+  module.exports = {
+    props: {
+      examdata: {
+        type: Array,
+        required: true
+      }
+    },
+    data() {
+        return {
+            searchQuery: ''
+        }
+    },
+    computed: {
+        filteredExams() {
+            const q = this.searchQuery.toLowerCase()
+            return this.examdata.filter(item => {
+                return item.sample_date.toLowerCase().includes(q) ||
+                       item.file.toLowerCase().includes(q) ||
+                       item.requested_by.toLowerCase().includes(q) ||
+                       item.assessmentSummary.join(', ').toLowerCase().includes(q);
+            });
+        }
+    },
+    methods: {
+        fileIcon(item) {
+            const extension = item.file.split('.').pop().toLowerCase()
+            return {
+                pdf: 'pdf.png',
+                xls: 'xls.png',
+                xlsx: 'xls.png',
+            }[extension] || 'xls.png'
+        }
+    },
+    components: {
+      'child-component': httpVueLoader('./ChildComponent.vue')
+    },
+  }
+</script>
+  
+<style>
+    input[type="text"] {
+        width: 100%;
+        padding: 10px;
+        margin-top: 10px;
+        box-sizing: border-box;
+        border: 2px solid #ccc;
+        border-radius: 4px;
+        background-color: #fff;
+        transition: border-color 0.3s;
+    }
+
+    input[type="text"]:focus {
+        border-color: #4CAF50; /* Highlight color on focus */
+    }
+
+    table {
+        width: 100%;
+        border-collapse: collapse;
+        margin-top: 10px;
+        font-size: 12px; /* Smaller font size for more content */
+        background: #f8f8f8; /* Light background for table */
+        border-radius: 8px; /* Rounded corners for table */
+    }
+
+    table, th, td {
+        border: 1px solid #ddd;
+    }
+
+    th, td {
+        text-align: left;
+        padding: 4px; /* Reduced padding for a more compact look */
+        border: 1px solid #ddd;
+    }
+
+    th {
+        background-color: #f0f8ff;
+    }
+
+    tr:nth-child(even) {
+        background-color: #f2f2f2;
+    }
+
+    /* Icon styling */
+    td img {
+        display: block;
+        width: 16px; /* Small icon size */
+        height: auto;
+        margin: 0 auto; /* Centering icon in the cell */
+    }
+</style>
---
+++ 5d2a48010f15903e74c855984fc7d3640d33737b/components/PatientHeader.vue
@@ -0,0 +1,61 @@
+<template>
+    <header>
+        <div class="header-content">
+            <div class="text-content">
+                <h1>Patient Clinical History</h1>
+                <h2>Basic Information</h2>
+                <p><strong>Name:</strong> John Doe</p>
+                <p><strong>Date of Birth:</strong> 2004-05-23</p>
+                <p><strong>Conditions:</strong> Autism Spectrum Disorder</p>
+            </div>
+            <img src="isaac.png" alt="Patient Photo" class="patient-photo">
+        </div>
+        <p>Quick overview and detailed medical records access.</p>
+    </header>
+  </template>
+  
+  <script>
+  module.exports = {
+    data() {
+        return {
+            who: 'world'
+        }
+    },
+  }
+  </script>
+  
+<style>
+    header, section {
+        margin-bottom: 20px;
+    }
+
+    /* Additional styles to ensure the header scales nicely */
+    header {
+        padding: 20px;
+        background-color: #f0f8ff;
+        border-bottom: 2px solid #ccc;
+    }
+
+    h1, h2 {
+        margin-top: 0;
+        color: #0056b3;
+        /* color: #fff; */
+    }
+
+    .header-content {
+        display: flex;
+        align-items: center; /* Aligns items vertically in the center */
+        justify-content: space-between; /* Space between text and image */
+    }
+
+    .text-content {
+        flex-grow: 1; /* Allows the text content to take up remaining space */
+    }
+
+    .patient-photo {
+        width: auto; /* Adjusts width automatically */
+        max-height: 200px; /* Adjusts the maximum height of the image */
+        border-radius: 10px; /* Rounds the corners of the image */
+        margin-left: 20px; /* Adds space between the text and the image */
+    }
+</style>
