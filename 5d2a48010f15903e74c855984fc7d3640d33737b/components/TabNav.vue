24d1cd0fc4749bf070e77031ef98d00ce374b4eb
Tony Lampada <tonylampada@gmail.com>
2024-08-10 22:29:56 -0300

    adiciona componente tabnav

---
+++ 5d2a48010f15903e74c855984fc7d3640d33737b/components/TabNav.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="tab-nav">
+    <ul class="tab-nav-list">
+      <li v-for="(tab, index) in tabNames" :key="index" @click="activeTab = index" :class="{ active: index === activeTab }">
+        {{ tab }}
+      </li>
+    </ul>
+    <div class="tab-content">
+      <slot :name="activeTab.toString()"></slot>
+    </div>
+  </div>
+</template>
+  
+<script>
+module.exports = {
+  props: {
+    tabNames: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      activeTab: 0
+    }
+  }
+}
+</script>
+  
+<style scoped>
+.tab-nav-list {
+  display: flex;
+  list-style: none;
+  padding: 0;
+}
+.tab-nav-list li {
+  padding: 10px;
+  cursor: pointer;
+}
+.tab-nav-list li.active {
+  font-weight: bold;
+}
+</style>
